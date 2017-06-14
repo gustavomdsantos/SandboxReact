d5e0a2f32e5ad2596ec994a356593b3f74b3818b
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-14 09:12:38 -0300

    Separa argumentos em variáveis separadas para melhor legibilidade

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
-ReactDOM.render(React.createElement(
-  'h1', null, 'Olá mundo em React!'
-), document.getElementsByClassName('container')[0]);
+var div_container = document.getElementsByClassName('container')[0];
+var helloReactElement = React.createElement('h1', null, 'Olá mundo em React!');
+
+ReactDOM.render(helloReactElement, div_container);
