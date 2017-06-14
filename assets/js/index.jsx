1be6844a7c04d1d0f7b7b36a4696e2a6438dede5
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-14 11:36:48 -0300

    Implementa exemplo de JSX das docs do React
    https://facebook.github.io/react/docs/introducing-jsx.html

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -1,4 +1,18 @@
 var div_container = document.getElementsByClassName('container')[0];
-var helloReactElement = <h1>Olá mundo em React!</h1>;
 
-ReactDOM.render(helloReactElement, div_container);
+const pessoa = {
+  nome: 'Gustavo',
+  sobrenome: 'Moraes'
+};
+
+function exibeNomePessoa(pessoa) {
+  return pessoa.nome + ' ' + pessoa.sobrenome;
+}
+
+const pessoaReactElement = (
+  <h1>
+    Olá, {exibeNomePessoa(pessoa)}!
+  </h1>
+);
+
+ReactDOM.render(pessoaReactElement, div_container);
