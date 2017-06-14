3c2aea8c02d87f0ed2c155685b44b69754c6fd70
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-14 11:56:43 -0300

    Adiciona arquivo `node web.js` para executar com "node-static"
    Arquivos JSX só funcionam em um servidor (mesmo estático), porque não são arquivos JavaScript.

---
+++ web.js
@@ -0,0 +1,20 @@
+var static = require('node-static');
+
+var _port = 9000;
+
+var file = new static.Server('./', {
+    cache: 0,
+    gzip: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+    }
+});
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(process.env.PORT || _port);
+
+console.log("Listening on port " + _port);
