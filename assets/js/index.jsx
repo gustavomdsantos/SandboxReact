7ee9a5d2510b87a561d462ad61257f4c7fb60a36
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-19 17:18:07 -0300

    Transforma um componente funcional React em uma classe-componente React

--- assets/js/index.jsx
+++ assets/js/index.jsx
@@ -13,19 +13,22 @@ function exibeNomePessoa(pessoa) {
  * Componente *funcional* React.
  * Renderiza uma mensagem de boas vindas para o nome passado como parâmetro.
  */
-function BemVindo(props) {
-	if (props.pessoa) {
+class BemVindo extends React.Component {
+
+	render() {
+		if (this.props.pessoa) {
+			return (
+				<div className="container">
+					<h1>Olá, {exibeNomePessoa(this.props.pessoa)}!</h1>
+				</div>
+			);
+		}
 		return (
 			<div className="container">
-				<h1>Olá, {exibeNomePessoa(props.pessoa)}!</h1>
+				<h1>Olá, estranho.</h1>
 			</div>
 		);
 	}
-	return (
-		<div className="container">
-			<h1>Olá, estranho.</h1>
-		</div>
-	);
 }
 
 const elemento = <BemVindo pessoa={gustavo} />;
