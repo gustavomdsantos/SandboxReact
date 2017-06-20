ca5fe1c6504af254a2d076f0c88d93991b2c520e
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-20 11:21:49 -0300

    Seta estilos CSS dentro do JSX

--- assets/js/view/voltar/VoltarView.jsx
+++ assets/js/view/voltar/VoltarView.jsx
@@ -1,14 +1,25 @@
 class VoltarView extends React.Component {
 
+  // Estas variáveis JSX será convertida para CSS dentro de `render()`
+  div_style = {
+    textAlign: "center"
+  };
+
+  a_style = {
+    cursor: "pointer"
+  };
+
   onClick$renderIndex() {
     ReactDOM.render(<Index />, document.getElementsByClassName('page')[0]);
   }
 
   render() {
     return (
-      <a onClick={this.onClick$renderIndex}>
-        Voltar ao menu principal
-      </a>
+      <div className="voltarView" style={this.div_style}>
+        <a style={this.a_style} onClick={this.onClick$renderIndex}>
+          Voltar ao menu principal
+        </a>
+      </div>
     );
   }
 }
