0409d017e8c70d4db02ed3b178cba40c3c7198d4
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-20 10:54:39 -0300

    Faz refatoração e otimização de código

--- assets/js/view/bemvindo/bemvindo.jsx
+++ assets/js/view/bemvindo/bemvindo.jsx
@@ -1,18 +1,32 @@
-function exibeNomePessoa(pessoa) {
-  return pessoa.nome + ' ' + pessoa.sobrenome;
-}
-
 /**
  * Componente *funcional* React.
  * Renderiza uma mensagem de boas vindas para o nome passado como parâmetro.
  */
 class BemVindo extends React.Component {
 
+  /** Esqueleto de objeto que deve ser passado como parâmetro. */
+  props = {
+    pessoa: {
+      nome: "",
+      sobrenome: ""
+    }
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+  }
+
+  exibeNomePessoa(pessoa) {
+    return pessoa.nome + ' ' + pessoa.sobrenome;
+  }
+
   render() {
     if (this.props.pessoa) {
       return (
         <div className="container">
-          <h1>Olá, {exibeNomePessoa(this.props.pessoa)}!</h1>
+          <h1>Olá, {this.exibeNomePessoa(this.props.pessoa)}!</h1>
         </div>
       );
     }
--- assets/js/view/relogio/relogio.jsx
+++ assets/js/view/relogio/relogio.jsx
@@ -1,5 +1,3 @@
-var div_page = document.getElementsByClassName('page')[0];
-
 function Relogio() {
   return (
     <div className="container">
