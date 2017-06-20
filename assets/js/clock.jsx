7b79f67e4ddc149636638409ec9528cd9c4a936d
Gustavo Moraes <gustavosotnas1@gmail.com>
2017-06-19 23:43:31 -0300

    Transforma um elemento React em um componente funcional React

--- assets/js/clock.jsx
+++ assets/js/clock.jsx
@@ -1,13 +1,18 @@
 var div_page = document.getElementsByClassName('page')[0];
 
-function tique() {
-  const elemento = (
+function Relogio(props) {
+  return (
     <div className="container">
       <h1>Olá mundo!</h1>
-      <h2>Agora são {new Date().toLocaleTimeString()}.</h2>
+      <h2>Agora são {props.horarioAtual.toLocaleTimeString()}.</h2>
     </div>
   );
-  ReactDOM.render(elemento, div_page);
+}
+
+function tique() {
+  ReactDOM.render(
+    <Relogio horarioAtual={new Date()} />,
+    div_page);
 }
 
 setInterval(tique, 1000);
